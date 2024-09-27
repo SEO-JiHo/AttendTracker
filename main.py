044f0043f6cdb93082479ba1190cf2065064bf49
SEO-Ji-Ho <jhseo9601@gmail.com>
2024-09-27 19:59:15 +0900

    ADB command(capturing screenshot function) added. scrolling down function added

--- main.py
+++ main.py
@@ -5,3 +5,30 @@
 #       'ㅇㅇㅇ님께서', '참석하셨습니다.', '참석을 취소하셨습니다.'
 #       9/26 == 9월 26일
 #       전체 멤버 -> pandas.Dataframe
+import subprocess
+import os
+import time
+from ocr import process_ocr
+
+adb_path = r'C:\Users\jhseo\Downloads\platform-tools-latest-windows\platform-tools\adb.exe'
+image_folder = "./screenshots-auto"
+
+def capture_screenshot(counter):
+
+    screenshot_name = f"screenshot_{counter}.png"
+    screenshot_path = os.path.join(image_folder, screenshot_name)
+
+    subprocess.run([adb_path, "shell", "screencap", "-p", f"/sdcard/screenshots_tmp/{screenshot_name}"])
+    subprocess.run([adb_path, "pull", f"/sdcard/screenshots_tmp/{screenshot_name}", screenshot_path])
+
+    return screenshot_path
+
+def scroll_down():
+    subprocess.run([adb_path, "shell", "input", "swipe", "500", "2300", "500", "400", "1100"])
+
+
+for i in range(3):
+    extrtacted_data = capture_screenshot(1)
+    print(process_ocr(extrtacted_data))
+    scroll_down()
+    time.sleep(0.5)
--- ocr.py
+++ ocr.py
@@ -2,43 +2,41 @@ import numpy as np
 from PIL import Image
 import pytesseract
 import cv2
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
+
+def process_ocr(image):
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+    img = Image.open(image)
+    img_np = np.array(img)
+
+    x_start, y_start, width, height = 200, 450, 800, 2400
+    cropped_img = img_np[y_start:y_start + height, x_start:x_start + width]
+
+    gray_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
+
+    custom_config = r'--oem 3 --psm 6'
+    text = pytesseract.image_to_string(cropped_img, config=custom_config, lang='kor')
+
+    return text
 
 
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
-
-img = Image.open('test_image.jpg')
-img_np = np.array(img)
-
-x_start, y_start, width, height = 200, 450, 800, 2400
-cropped_img = img_np[y_start:y_start + height, x_start:x_start + width]
-
-gray_img = cv2.cvtColor(cropped_img, cv2.COLOR_BGR2GRAY)
-
-custom_config = r'--oem 3 --psm 6'
-text = pytesseract.image_to_string(gray_img, config=custom_config, lang='kor')
-
-print("OCR Result:")
-print(text)
-
-
-#---------------------------
-plt.figure(figsize=(12, 10))
-
-plt.subplot(1, 3, 1)
-plt.title('Original Image')
-plt.imshow(img_np)
-plt.axis('off')
-
-plt.subplot(1, 3, 2)
-plt.title('Cropped Image')
-plt.imshow(cropped_img)
-plt.axis('off')
-
-plt.subplot(1, 3, 3)
-plt.title('Morphed Image')
-plt.imshow(gray_img)
-plt.axis('off')
-
-plt.tight_layout()
-plt.show()
+# plt.figure(figsize=(12, 10))
+#
+# plt.subplot(1, 3, 1)
+# plt.title('Original Image')
+# plt.imshow(img_np)
+# plt.axis('off')
+#
+# plt.subplot(1, 3, 2)
+# plt.title('Cropped Image')
+# plt.imshow(cropped_img)
+# plt.axis('off')
+#
+# plt.subplot(1, 3, 3)
+# plt.title('gray Image')
+# plt.imshow(gray_img)
+# plt.axis('off')
+#
+# plt.tight_layout()
+# plt.show()
